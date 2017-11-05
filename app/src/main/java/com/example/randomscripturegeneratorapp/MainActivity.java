14f6834d8d3236075f1c594604538b23435eaa58
tylonator6 <bra14038@byui.edu>
2017-11-05 09:35:01 -0700

    Added Pure and Weighted Drop down on Main Activity

--- app/src/main/java/com/example/randomscripturegeneratorapp/MainActivity.java
+++ app/src/main/java/com/example/randomscripturegeneratorapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 //CHANGE: Hi Vlad!
 
@@ -27,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         context = this.getApplicationContext();
         // deserialize JSON file to Java array
         WorkWithJSON.deserializeJSON(context);
+
+        Spinner spinner = (Spinner) findViewById(R.id.pure_weighted_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.pure_weighted_random, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     public void sendVerseToDisplay(View view) {
