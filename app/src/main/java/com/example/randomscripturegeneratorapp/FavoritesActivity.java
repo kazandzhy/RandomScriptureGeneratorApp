2407ed56ee329dfd884210fbc24086759f9878ea
Vlad <vlad_kazandzhy@ukr.net>
2017-11-29 00:32:46 -0700

    Started to work with delete Favorites

--- app/src/main/java/com/example/randomscripturegeneratorapp/FavoritesActivity.java
+++ app/src/main/java/com/example/randomscripturegeneratorapp/FavoritesActivity.java
@@ -7,10 +7,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -30,6 +32,7 @@ public class FavoritesActivity extends AppCompatActivity {
 
     public static ScriptureData[] favoritesArray;
     private static Context context;
+    private Boolean noFavorites = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,39 @@ public class FavoritesActivity extends AppCompatActivity {
         //Populate the listView
         populateFavoritesList();
 
+        final ListView favoritesList = (ListView) findViewById(R.id.favorites);
+
+        favoritesList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+                String selectedFromList =(String) (favoritesList.getItemAtPosition(myItemInt));
+                registerForContextMenu(myView);
+                Log.i("selected is ", selectedFromList);
+                return true;
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_favorites_longclick, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_deleteFavorite:
+                // code to delete favorite
+                Log.i("delete favorites ", "works");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,8 +117,7 @@ public class FavoritesActivity extends AppCompatActivity {
                 if (response.equals("No favorites")) {
                     Toast toast = Toast.makeText(getApplicationContext(), "You have no Favorites saved yet.", Toast.LENGTH_LONG);
                     toast.show();
-
-                    startActivity(new Intent(context, MainActivity.class));
+                    noFavorites = true;
                 } else {
                     //create Gson instance
                     Gson gson = new Gson();
@@ -135,6 +170,9 @@ public class FavoritesActivity extends AppCompatActivity {
             editor.apply();
 
             startActivity(displayIntent);
+        } else if (noFavorites) {
+            Toast toast = Toast.makeText(getApplicationContext(), "You have no Favorites saved yet.", Toast.LENGTH_SHORT);
+            toast.show();
         } else {
             Toast toast = Toast.makeText(getApplicationContext(), "Please wait while Favorites load.", Toast.LENGTH_SHORT);
             toast.show();
