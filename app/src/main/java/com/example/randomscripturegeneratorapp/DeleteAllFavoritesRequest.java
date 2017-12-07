592df78cd6250bbe9229bdf748fb728f37e4a439
Vlad <vlad_kazandzhy@ukr.net>
2017-12-07 12:37:15 -0700

    Added JavaDoc and code comments for AddToFavoritesRequest, DeleteAllFavoritesRequest, and DeleteFavoriteRequest

--- app/src/main/java/com/example/randomscripturegeneratorapp/DeleteAllFavoritesRequest.java
+++ app/src/main/java/com/example/randomscripturegeneratorapp/DeleteAllFavoritesRequest.java
@@ -8,25 +8,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Vlad on 01.12.2017.
+ * Class to send delete all favorites request to database
+ *
+ * This class helps with sending user's delete all verses request to Volley, which connects this info
+ * with the database used at www.randomscriptureverse.com
+ *
+ * @author Vlad Kazandzhy, Nathan Tagg, Tyler Braithwaite
  */
 
 public class DeleteAllFavoritesRequest extends StringRequest {
-    private static final String REMOVE_FAVORITE_REQUEST_URL = "http://www.randomscriptureverse.com/php/deleteAllFavoritesApp.php";
+    // URL for connecting user information with the website database
+    private static final String REMOVE_FAVORITES_REQUEST_URL = "http://www.randomscriptureverse.com/php/deleteAllFavoritesApp.php";
     // create map params
     private Map<String, String> params;
 
+    /**
+     * This function posts the one necessary variable (user_id) to the PHP file
+     * using Volley
+     *
+     * @param user_id
+     * @param listener
+     */
     public DeleteAllFavoritesRequest(String user_id, Response.Listener<String> listener) {
-        super(Request.Method.POST, REMOVE_FAVORITE_REQUEST_URL, listener, null);
-        // this map allows volley to pass information from DeleteAllFavoritesRequest to deleteAllFavoritesApp.php
+        super(Request.Method.POST, REMOVE_FAVORITES_REQUEST_URL, listener, null);
+        // this map allows Volley to pass information from DeleteAllFavoritesRequest to deleteAllFavoritesApp.php
         params = new HashMap<>();
         params.put("user_id", user_id);
     }
 
-    // Volley accesses this data
+    /**
+     * Volley accesses data in map
+     *
+     * @return parameters of user
+     */
     @Override
     public Map<String, String> getParams() {
-        // get params returns parameters of user
         return params;
     }
 }
